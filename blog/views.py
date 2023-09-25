e04aee577db01f6e2b8d0f2205897caab7890ed2
urmat <urmat.0609@gmail.com>
2023-09-25 23:36:44 +0600

    add samples is very nice

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
 # Create your views here.
+
+
+def post_detail(request, id):
+    try:
+        post = get_object_or_404(Post,
+                                 id=id,
+                                 status=Post.Status.PUBLISHED)
+    except Post.DoesNotExist:
+        raise Http404("No Post Found")
+
+    return render(request,"blog/detail.html",{'post': post})
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,"blog/list.html",{'posts': posts})
